2de1ed91cfa024f3d7d4ebad82b0df6fefee4c5c
sariomari <73112567+sariomari@users.noreply.github.com>
2021-12-24 19:18:49 +0200

    just some testing

--- connect.py
+++ connect.py
@@ -1,15 +1,22 @@
 import mysql.connector
 import urllib
+import requests
 
 def main():
     conn = mysql.connector.connect(host="localhost", username="DbMysql48", password="DbMysql48", database="DbMysql48",
                                    port=3305)
+    url = "https://imdb-api.com/en/API/Top250Movies/k_6tj9fsmd"
+    payloads, headers = {}, {}
+    res = requests.request("GET", url=url, data=payloads, headers=headers)
+    print(res.text.encode("utf8"))
     print(conn)
     mdb = conn.cursor()
     mdb.execute("SELECT * FROM HW_Group")
     res = mdb.fetchall()
     for x in res:
         print(x)
+
+    
 
 
 main()
