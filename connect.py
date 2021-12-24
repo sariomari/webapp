5dfe5e87bd18cdb0c8c36c3c8550cf425eef66d0
sariomari <73112567+sariomari@users.noreply.github.com>
2021-12-24 11:03:25 +0200

    connection test (succeeded)

--- connect.py
+++ connect.py
@@ -2,4 +2,14 @@ import mysql.connector
 import urllib
 
 def main():
-    pass
+    conn = mysql.connector.connect(host="localhost", username="DbMysql48", password="DbMysql48", database="DbMysql48",
+                                   port=3305)
+    print(conn)
+    mdb = conn.cursor()
+    mdb.execute("SELECT * FROM HW_Group")
+    res = mdb.fetchall()
+    for x in res:
+        print(x)
+
+
+main()
