2a3d2a0fd91e07e4f970b520da4a0352021b245c
sariomari <73112567+sariomari@users.noreply.github.com>
2021-12-26 12:48:48 +0200

    html changes with bootstrap

--- connect.py
+++ connect.py
@@ -3,20 +3,30 @@ import mysql.connector
 import requests
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/about')
 def about():
-    return 'About Page'
+    return render_template('about.html')
+
+
 def main():
-    conn = mysql.connector.connect(host="localhost", username="DbMysql48", password="DbMysql48", database="DbMysql48",
-                                   port=3305)
-    url = "https://imdb-api.com/en/API/Top250Movies/k_6tj9fsmd"
+    conn = mysql.connector.connect(host="localhost", username="DbMysql48", password="DbMysql48", database="DbMysql48", port=3305)
+    if conn:
+        print('Successfully Connected to DbMysql48.')
+    else:
+        print('Connection to DbMysql48 Failed.')
+    
+    url = 'https://imdb-api.com/en/API/Top250Movies/k_6tj9fsmd'
+
     payloads, headers = {}, {}
+
     res = requests.request("GET", url=url, data=payloads, headers=headers)
+
     print(res.text.encode("utf8"))
     print(conn)
     mdb = conn.cursor()
