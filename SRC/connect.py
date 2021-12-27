423755ba3e9e336e707bb5a69f5fad62a3295447
sariomari <73112567+sariomari@users.noreply.github.com>
2021-12-27 20:21:27 +0200

    new structure

--- SRC/connect.py
+++ SRC/connect.py
@@ -15,28 +15,28 @@ def about():
 
 
 def main():
-    conn = mysql.connector.connect(host="localhost", username="DbMysql48", password="DbMysql48", database="DbMysql48", port=3305)
-    if conn:
-        print('Successfully Connected to DbMysql48.')
-    else:
-        print('Connection to DbMysql48 Failed.')
-    
+    # conn = mysql.connector.connect(host="localhost", username="DbMysql48", password="DbMysql48", database="DbMysql48", port=3305)
+
+    # if conn:
+    #     print('Successfully Connected to DbMysql48.')
+    # else:
+    #     print('Connection to DbMysql48 Failed.')
+    #     return -1
+    # my_cursor = conn.cursor()
+    # my_cursor.execute("CREATE TABLE top250")
     url = 'https://imdb-api.com/en/API/Top250Movies/k_6tj9fsmd'
-
     payloads, headers = {}, {}
-
     res = requests.request("GET", url=url, data=payloads, headers=headers)
+    top250_json=res.json()
 
-    print(res.text.encode("utf8"))
-    print(conn)
-    mdb = conn.cursor()
-    mdb.execute("SELECT * FROM HW_Group")
-    res = mdb.fetchall()
-    for x in res:
-        print(x)
+    for i in range(5):
+        print(top250_json["items"][i])
+
+
 
     
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    print(main())
